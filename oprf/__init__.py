1fec2adeabb79462362c9e0a9e5589560ff8ad9e
Sidd <siddharthmittal95@yahoo.co.in>
2024-10-10 13:18:02 +0200

    MAJOR: Deleted everything obsolete from main

--- oprf/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import standard
-from .standard import *
-
-from . import external
-from .external import *
-
-from . import analysis
-from .analysis import *
--- oprf/standard/prf_receptive_field_response.py
+++
@@ -1,6 +0,0 @@
-class ReceptiveFieldResponse:
-    def __init__(self, row, col, timecourse):
-        self.row = row
-        self.col= col
-        self.timecourse = timecourse
-        self.best_fit_info = None
