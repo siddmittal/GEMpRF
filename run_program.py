ac9aca64acee30ff1884939d9cade05d982b18bd
Siddharth Mittal <siddharthmittal95@yahoo.co.in>
2025-04-09 13:28:10 +0200

    Minor: basic management of program flow

--- run_program.py
+++ run_program.py
@@ -33,15 +33,14 @@ def run_selected_program(selected_program, config_filepath, spatial_points_xy = 
     _ = ggm(default_gpu_id = int(cfg.gpu['default_gpu']))
 
     # copy the config file to the results folder
-    result_dir = os.path.join(cfg.bids.get("basepath"), "derivatives", "prfanalyze-gem", f'analysis-{cfg.bids.get("results_anaylsis_id")}')
-    shutil.copy(config_filepath, result_dir) if os.makedirs(result_dir, exist_ok=True) is None else None
+    if selected_program == SelectedProgram.GEMAnalysis:
+        result_dir = os.path.join(cfg.bids.get("basepath"), "derivatives", "prfanalyze-gem", f'analysis-{cfg.bids.get("results_anaylsis_id")}')
+        shutil.copy(config_filepath, result_dir) if os.makedirs(result_dir, exist_ok=True) is None else None
 
     # ...prf spatial points
     if spatial_points_xy is None:
         if selected_program == SelectedProgram.GEMAnalysis:
             spatial_points_yx = GEMpRFAnalysis.get_prf_spatial_points(cfg) # this will return the spatial points in (row/y, col/x) format
-        elif selected_program == SelectedProgram.GEMSimulations:
-            spatial_points_yx = GEMSimulationsRunner.get_prf_spatial_points(cfg)
 
         # convert spatial points from (row, col) to (col, row)
         spatial_points_xy = spatial_points_yx
@@ -55,8 +54,6 @@ def run_selected_program(selected_program, config_filepath, spatial_points_xy = 
     # additional dimensions
     if selected_program == SelectedProgram.GEMAnalysis:
         additional_dimensions = GEMpRFAnalysis.get_additional_dimensions(cfg, selected_prf_model)
-    elif selected_program == SelectedProgram.GEMSimulations:
-        additional_dimensions = GEMSimulationsRunner.get_additional_dimensions(cfg, selected_prf_model)
         
     prf_space = PRFSpace(spatial_points_xy, additional_dimensions=additional_dimensions)
     prf_space.convert_spatial_to_multidim()
@@ -65,15 +62,15 @@ def run_selected_program(selected_program, config_filepath, spatial_points_xy = 
     # run the pRF analysis
     if selected_program == SelectedProgram.GEMAnalysis:             
         GEMpRFAnalysis.run(cfg, prf_model, prf_space)
-    elif selected_program == SelectedProgram.GEMSimulations:
-        GEMSimulationsRunner.run(cfg, prf_model, prf_space)
         
 
 ################################################---------------------------------MAIN---------------------------------################################################
 # run the main function
-if __name__ == "__main__":    
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+if __name__ == "__main__":            
+    # NOTE: Select the program to run
+    # selected_program = SelectedProgram.GEMAnalysis
     selected_program = SelectedProgram.GEMAnalysis
+
     print ("Running the GEM pRF Analysis...")
     # from gem.run.run_gem_prf_analysis import GEMpRFAnalysis
     # GEMpRFAnalysis.run()    
@@ -83,19 +80,24 @@ if __name__ == "__main__":
     # profiler = cProfile.Profile()
     # profiler.enable()
 
-    # ...simulations
-    if selected_program == SelectedProgram.GEMSimulations:
-        from raven.simulation.run.run_gem_simulations import GEMSimulationsRunner
-        # NOTE: KDE computation Configs
-        config_filepath = os.path.join(script_dir, 'raven', 'configs', 'simulations_configs', '0002_kde_generation-voxel-394-specific_config.xml')
-        # config_filepath = os.path.join(script_dir, '..', 'configs', 'simulations_configs', '0001_kde_generation_config.xml') # simulations_config.xml # david_001_hcp_stimuli_simulations_config # 0001_kde_generation_config
-        
-        # NOTE: Stimuli Comparison Configs
-        # config_filepath = os.path.join(script_dir, '..', 'configs', 'stimuli_comparison_configs', 'num-001_dataset-hcpret_task-bars_purpose-stimulicomparison_remarks-david.xml') 
-        # config_filepath = os.path.join(script_dir, '..', 'configs', 'stimuli_comparison_configs', 'num-002_dataset-hcpret_task-wedgering_purpose-stimulicomparison_remarks-david.xml') 
-        # config_filepath = os.path.join(script_dir, '..', 'configs', 'stimuli_comparison_configs', 'num-003_dataset-hcpret_task-barwedgering_purpose-stimulicomparison_remarks-david.xml')     
-    elif selected_program == SelectedProgram.GEMAnalysis:
-        config_filepath = os.path.join(os.path.dirname(__file__), 'gem', 'configs', 'analysis_configs', 'analysis_config - VerifyChanges.xml')
+    # Check if the user has provided a config filepath
+    has_user_provided_config = False
+    try:
+        config_filepath = sys.argv[1]
+        has_user_provided_config = True
+        if selected_program == SelectedProgram.GEMSimulations:
+            pass
+    except IndexError:
+        pass
+
+    # ...Set config paths
+    if not has_user_provided_config:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        if selected_program == SelectedProgram.GEMSimulations:
+            pass
+        elif selected_program == SelectedProgram.GEMAnalysis:
+            # config_filepath = os.path.join(os.path.dirname(__file__), 'gem', 'configs', 'analysis_configs', 'analysis_config - VerifyChanges.xml')
+            config_filepath = os.path.join(os.path.dirname(__file__), 'gem', 'configs', 'analysis_configs', 'analysis_config_retcomp17BIDS_wedgeLR-VerfiyDefaultGPU.xml')
         
     # GEMSimulationsRunner.run(config_filepath, isSimulation=IS_SIMULATION)
     run_selected_program(selected_program, config_filepath)
