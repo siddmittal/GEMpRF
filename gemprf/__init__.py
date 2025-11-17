97887e6fc331a11de77c6fd5600b34dc922047f2
Siddharth Mittal <siddharthmittal95@yahoo.co.in>
2025-11-17 19:26:53 +0100

    resolvinig circular dependencies due to wrapper

--- gemprf/__init__.py
+++ gemprf/__init__.py
@@ -1,7 +1,23 @@
-import sys
+"""
+"@Author  :   Siddharth Mittal",
+"@Version :   1.0",
+"@Contact :   siddharth.mittal@meduniwien.ac.at",
+"@License :   (C)Copyright 2025, Medical University of Vienna",
+"@Desc    :   None",
+
+"""
+
+# gemprf/__init__.py
+
 import importlib
 
-_real_gem = importlib.import_module("gem")
-_real_gem.__gemprf_version__ = "0.1.3" 
+# Load real gem package lazily through importlib
+_gem = importlib.import_module("gem")
 
-sys.modules[__name__] = _real_gem
+# Expose selected API from gemprf (not gem)
+__all__ = ["run", "__version__"]
+
+def run(*args, **kwargs):
+    return _gem.run(*args, **kwargs)
+
+__version__ = "0.1.4"
